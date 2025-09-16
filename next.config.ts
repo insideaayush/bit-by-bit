ea7d535c60c3f3e18c1bb67129decde3f52c3adc
Aayush Gautam <aayush.gautam924@gmail.com>
2025-09-16 12:40:12 +0800

    fix base path issues for css files

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,8 @@
 import type { NextConfig } from "next";
 
-const base = process.env.NEXT_PUBLIC_BASE_PATH || ''
 
 const nextConfig: NextConfig = {
   output: 'export',
-  // basePath: base,
-  // assetPrefix: base,
   images: {
     unoptimized: true,
   },
