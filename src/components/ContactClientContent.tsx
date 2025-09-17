5835236e7980d463dc39bcde34c63ffb31d3d263
Aayush Gautam <aayush.gautam924@gmail.com>
2025-09-17 18:40:10 +0800

    feat: update share icon on contact page

--- src/components/ContactClientContent.tsx
+++ src/components/ContactClientContent.tsx
@@ -29,9 +29,7 @@ const ContactClientContent: React.FC<ContactClientContentProps> = ({ data }) => 
       >
         {showTerminal ? (
           // Share Icon (simple SVG) - shown when currently in Terminal View, clicking switches to Share
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`${styles.toggleIcon} ${!showTerminal ? styles.rotated : ''}`}>
-            <path d="M18 16.08c-.76 0-1.44.3-1.96.77L8.91 12.7c.05-.23.09-.46.09-.7s-.04-.47-.09-.7l7.05-4.11c.52.48 1.2.78 1.96.78 1.66 0 3-1.34 3-3s-1.34-3-3-3-3 1.34-3 3c0 .24.04.47.09.7L7.05 11.9c-.52-.48-1.2-.78-1.96-.78-1.66 0-3 1.34-3 3s1.34 3 3 3c.76 0 1.44-.3 1.96-.77L15.09 16.7c-.05.23-.09.46-.09.7 0 1.66 1.34 3 3 3s3-1.34 3-3-1.34-3-3-3z"/>
-          </svg>
+          <Image src="/icons/share.svg" alt="Share Info" width={24} height={24} className={`${styles.toggleIcon} ${!showTerminal ? styles.rotated : ''}`} />
         ) : (
           // Terminal Icon (simple SVG) - shown when currently in Share Info, clicking switches to Terminal
           <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className={`${styles.toggleIcon} ${showTerminal ? styles.rotated : ''}`}>
